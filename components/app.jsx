d6801ba1aef2092918b9cc17aff3f2c6fa2f16a0
Nazar Vinnichuk <nazar@vinnich.uk>
2024-07-01 22:14:07 +0300

    Expand the content.

--- components/app.jsx
+++ components/app.jsx
@@ -3,22 +3,34 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
+  Stack,
   ThemeProvider,
+  Typography,
   createTheme,
 } from "@mui/material";
 import {StrictMode, useState} from "react";
 
 export const App = () => {
   const [article, setArticle] = useState("die");
-  const onArticleChange = (event) => setArticle(event.target.value);
+  const [word, setWord] = useState("Apfel");
 
   return (
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <RadioGroup row value={article} onChange={onArticleChange}>
-          {["der", "die", "das"].map(makeRadio)}
-        </RadioGroup>
+        <Stack gap={2} px={2} py={2} alignItems="center">
+          <Typography>
+            Wie heißt
+            <span lang="en-us"> apple </span>
+            auf Deutsch?
+          </Typography>
+          <RadioGroup row value={article} onChange={makeListener(setArticle)}>
+            {["der", "die", "das"].map(makeRadio)}
+          </RadioGroup>
+          <RadioGroup row value={word} onChange={makeListener(setWord)}>
+            {["Apfel", "Geschirrreiniger", "Kühlschrank"].map(makeRadio)}
+          </RadioGroup>
+        </Stack>
       </ThemeProvider>
     </StrictMode>
   );
@@ -41,3 +53,5 @@ const makeRadio = (value) => (
 );
 
 const radio = <Radio />;
+
+const makeListener = (setter) => (event) => setter(event.target.value);
