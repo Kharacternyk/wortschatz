d5a8dd552802712512d5b6c15cdc93750bc001d3
Nazar Vinnichuk <nazar@vinnich.uk>
2024-07-07 23:11:06 +0300

    Switch to the production data.

--- functions/nouns.js
+++ functions/nouns.js
@@ -1,21 +1,9 @@
 export const onRequestGet = async (context) => {
-  const writeRecords = [
-    "Time	Die Zeit	Die Zeiten",
-    "Man	Der Mann	Die Männer",
-    "Hand	Die Hand	Die Hände",
-    "Day	Der Tag	Die Tage",
-    "Way	Der Weg	Die Wege",
-  ];
-  const writePromises = [context.env.nouns.put("length", writeRecords.length)];
-  for (let index = 0; index < writeRecords.length; ++index) {
-    writePromises.push(context.env.nouns.put(index, writeRecords[index]));
-  }
-
-  await Promise.all(writePromises);
-
-  const length = await context.env.nouns.get("length", {
-    cacheTtl: 3600,
-  });
+  const length = Number(
+    await context.env.nouns.get("length", {
+      cacheTtl: 300,
+    })
+  );
   const readPromises = [];
   const keys = [];
 
