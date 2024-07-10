4678ea50dcb8824b09ed55d365a1123a16cc4cc5
Nazar Vinnichuk <nazar@vinnich.uk>
2024-07-10 20:56:47 +0300

    Add some credits.

--- components/app.jsx
+++ components/app.jsx
@@ -1,9 +1,11 @@
 import {
   CssBaseline,
+  Link,
   Paper,
   Stack,
   SvgIcon,
   ThemeProvider,
+  Typography,
   createTheme,
 } from "@mui/material";
 import {StrictMode} from "react";
@@ -15,12 +17,31 @@ export const App = () => {
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Paper sx={paperStyle}>
-          <Stack gap={2} px={2} py={2} alignItems="center">
-            <SvgIcon inheritViewBox component={Logo} sx={logoStyle} />
-            <Form />
-          </Stack>
-        </Paper>
+        <Stack
+          gap={1}
+          p={2}
+          alignItems="stretch"
+          height="100vh"
+          justifyContent="space-between"
+        >
+          <Paper>
+            <Stack gap={2} p={2} alignItems="center">
+              <SvgIcon inheritViewBox component={Logo} sx={logoStyle} />
+              <Form />
+            </Stack>
+          </Paper>
+          <Typography align="center" variant="caption">
+            ©{" "}
+            <Link underline="hover" href="https://vinnich.uk">
+              https://vinnich.uk
+            </Link>{" "}
+            (diese Seite) <br />©{" "}
+            <Link underline="hover" href="https://frequencylists.blogspot.com/">
+              https://frequencylists.blogspot.com/
+            </Link>{" "}
+            (die Wörter)
+          </Typography>
+        </Stack>
       </ThemeProvider>
     </StrictMode>
   );
@@ -31,10 +52,6 @@ const theme = createTheme({
     mode: "dark",
   },
 });
-
-const paperStyle = {
-  m: 2,
-};
 
 const logoStyle = {
   height: 64,
